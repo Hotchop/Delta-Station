b15e574ea4eb66c9a57022436ce15ef7429b2f98
Hotchop <mirandanic1996@gmail.com>
2024-01-12 00:18:02 -0300

    Added all base station parts

--- Global/Cursor.gd
+++ Global/Cursor.gd
@@ -6,8 +6,7 @@ var cursor_h = preload("res://Assets/UI/Cursor-Hover.png")
 
 func default_cursor():
 	Input.set_custom_mouse_cursor(cursor,Input.CURSOR_ARROW,Vector2(20,20))
-	print("Cursor")
 	
 func hover_cursor():
 	Input.set_custom_mouse_cursor(cursor_h,Input.CURSOR_ARROW,Vector2(20,20))
-	print("Hover")
+	
